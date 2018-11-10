1ce423bd5a07be523a0075767df82b97a877cd9f
MITHU <44640213+awakemithu@users.noreply.github.com>
2018-11-11 03:21:18 +0600

    Update mod_helloworld.php
    This file is the main entry point for the module. It will perform any necessary initialization routines, call helper routines to collect any necessary data, and include the template which will display the module output.

--- mod_helloworld.php
+++ mod_helloworld.php
@@ -1,2 +1,2 @@
 
-//This file is the main entry point for the module. It will perform any necessary initialization routines, call helper routines to collect any necessary data, and include the template which will display the module output.
+//AAA
